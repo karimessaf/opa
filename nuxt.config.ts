75eef725b39b4a391e92e7a485cfc06c43254a60
karimessaf <karim.essaf@gmail.com>
2025-04-05 22:44:05 +0200

    feat(eslint): use stylistic and fix offenses

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/ui',
+  ],
   devtools: { enabled: true },
 
   app: {
     head: {
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-      ]
-    }
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
   },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/ui'
-  ],
-  
-  css: [ '~/assets/main.css' ]
+  css: ['~/assets/main.css'],
+  compatibilityDate: '2024-11-01',
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
