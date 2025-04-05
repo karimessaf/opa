f579dfe6e5b1f136ddeb30db43132ab066d0bb59
karimessaf <karim.essaf@gmail.com>
2025-04-05 23:10:19 +0200

    feat(asset): add cat icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,9 @@ export default defineNuxtConfig({
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/cat.ico' },
+      ],
     },
   },
 
