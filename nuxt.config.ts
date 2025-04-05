5c01dcac8fd40b5f4b054832405992eb5fc22861
karimessaf <karim.essaf@gmail.com>
2025-04-05 22:23:38 +0200

    feat(tailwind): config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,20 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/ui'
-  ]
+  ],
+  
+  css: [ '~/assets/main.css' ]
 })
