9ac6b7ea2dd12a2cdf4026ef4895fc33c44224f7
karimessaf <karim.essaf@gmail.com>
2025-04-07 11:57:09 +0200

    chore: remove comment

--- app.vue
+++ app.vue
@@ -1,7 +1,6 @@
 <template>
   <UApp class="min-h-screen">
     <UContainer class="min-h-screen">
-      <!-- https://nuxt.com/docs/guide/directory-structure/layouts -->
       <NuxtLayout>
         <NuxtPage />
       </NuxtLayout>
