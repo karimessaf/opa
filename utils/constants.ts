bef9fb4ca6ced85ef5fc70ae49c6ccffa3dd36fd
karimessaf <karim.essaf@gmail.com>
2025-04-24 22:25:27 +0200

    chore(util): add constants

---
+++ utils/constants.ts
@@ -0,0 +1,29 @@
+export const POSSIBLE_NAMES = [
+  'Agathos',
+  'Αγαθός',
+  'Disco chef',
+  'Ντίσκο σεφ',
+  'Nicholas',
+  'Νικόλαος',
+  'Nicholas Agathos',
+  'Nikos Agathos',
+  'Νικόλαος Αγαθός',
+  'Niko',
+  'Νίκο',
+  'Nikos',
+  'Νίκος',
+]
+
+export const RESPONSE_BY_NAME: Record<string, string[]> = {
+  'the OGs': ['vic', 'victor', 'matt', 'matthieu', 'vlad', 'clo', 'clothilde'],
+  'wrong dev': ['dayana', 'rado', 'radoslav', 'georgi'],
+  'you don\'t expect me to do them all...?': ['corentin', 'aurélie', 'aurelie', 'margo'],
+  'you whish!': ['jean-luc', 'jean luc', 'jeanluc', 'picard'],
+  'Hees': ['jim'],
+  'POPO!': ['pauline'],
+  '👀': ['gregoire', 'grégoire', 'philippe', 'jb', 'jean-baptiste', 'jean baptiste'],
+  'our amazing <span class="line-through text-xs">designer</span> PM! but that\'s not you, is it?': ['aurora'],
+  'THE WHOLE COMPANY?': ['smovin'],
+}
+
+export const TOTAL_EASTER_EGGS = 14
