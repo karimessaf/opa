de66116299f991befb615c222c98aaa2b01a7e53
karimessaf <karim.essaf@gmail.com>
2025-04-24 22:23:46 +0200

    feat(app): set background image

--- app.vue
+++ app.vue
@@ -1,9 +1,33 @@
 <template>
   <UApp class="min-h-screen">
-    <UContainer class="min-h-screen">
-      <NuxtLayout>
-        <NuxtPage />
-      </NuxtLayout>
-    </UContainer>
+    <div
+      class="min-h-screen w-full relative"
+      :class="{
+        [`bg-[url('/images/athens.jpg')] bg-cover bg-center bg-fixed`]: mounted && hasSetBackgroundImage,
+      }"
+    >
+      <div
+        v-if="mounted && hasSetBackgroundImage"
+        class="absolute inset-0 backdrop-blur-sm bg-black/20"
+      />
+      <UContainer class="min-h-screen relative">
+        <NuxtLayout>
+          <NuxtPage />
+        </NuxtLayout>
+      </UContainer>
+    </div>
   </UApp>
 </template>
+
+<script setup lang="ts">
+import { useStorage } from '@vueuse/core'
+import { ref, onMounted } from 'vue'
+
+const hasSetBackgroundImage = useStorage('hasSetBackgroundImage', false)
+
+const mounted = ref(false)
+
+onMounted(() => {
+  mounted.value = true
+})
+</script>
